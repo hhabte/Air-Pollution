fe116482d4ebbfe7d283cf1679c5c5c1c5a96d6f
hhabte <hanaghabte@gmail.com>
2016-06-26 12:21:16 -0700

    Create pollutantmean.R

---
+++ pollutantmean.R
@@ -0,0 +1,11 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  files_full <- list.files(directory, full.names = TRUE) 
+  
+  data <- data.frame()
+  
+  for (i in id) {
+    data <- rbind(data, read.csv(files_full[i]))
+  }
+  
+  mean(data[, pollutant], na.rm = TRUE)
+}
